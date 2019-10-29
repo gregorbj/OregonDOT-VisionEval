5ebaacb28fac8eb9263f92ce112f4d8c58e3d739
gregorbj <gregorbj@gmail.com>
2019-10-29 11:57:31 -0700

    Remove ModelType argument because no longer necessary

--- sources/CalcOregonMetroMeasures.R
+++ sources/CalcOregonMetroMeasures.R
@@ -2,13 +2,13 @@ library(visioneval)
 library(VEReports)
 source("CalcMetroMeasuresFunction.R")
 Ma <- c("Albany", "Bend", "Corvallis", "EugeneSpringfield", "GrantsPass", 
-        "Metro", "RogueValley", "SalemKeizer")
+        "Metro", "RogueValley", "SalemKeizer", "None")
 print("Calculating metropolitan area measures for 2005")
-write.csv(calcMetropolitanMeasures(Year = "2005", Ma = Ma, ModelType = "VE-State"),
+write.csv(calcMetropolitanMeasures(Year = "2005", Ma = Ma), row.names = FALSE,
           file = "metro_measures_2005.csv")
 print("Calculating metropolitan area measures for 2010")
-write.csv(calcMetropolitanMeasures(Year = "2010", Ma = Ma, ModelType = "VE-State"),
+write.csv(calcMetropolitanMeasures(Year = "2010", Ma = Ma), row.names = FALSE,
           file = "metro_measures_2010.csv")
 print("Calculating metropolitan area measures for 2040")
-write.csv(calcMetropolitanMeasures(Year = "2040", Ma = Ma, ModelType = "VE-State"),
+write.csv(calcMetropolitanMeasures(Year = "2040", Ma = Ma), row.names = FALSE,
           file = "metro_measures_2040.csv")
