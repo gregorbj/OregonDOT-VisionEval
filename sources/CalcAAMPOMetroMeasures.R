ee489bcf6f5193385bd17b9f491f1699fdb83d99
gregorbj <gregorbj@gmail.com>
2019-10-29 11:51:07 -0700

    Change call to calcMetropolitanMeasures function to eliminate ModelType argument because no longer used. Correct output file name.

--- sources/CalcAAMPOMetroMeasures.R
+++ sources/CalcAAMPOMetroMeasures.R
@@ -2,5 +2,5 @@ library(visioneval)
 library(VEReports)
 source("CalcMetroMeasuresFunction.R")
 Ma <- c("AAMPO")
-write.csv(calcMetropolitanMeasures(Year = "2010", Ma = Ma, ModelType = "VE-RSPM"),
-          file = "metro_measures_2005.csv")
+write.csv(calcMetropolitanMeasures(Year = "2010", Ma = Ma), row.names = FALSE,
+          file = "metro_measures_2010.csv")
